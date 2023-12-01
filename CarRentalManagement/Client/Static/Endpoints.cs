87df84474a5bde5f2ad0509010cf69c14e63b092
GOH JONAH 2204785F <2204785F@student.tp.edu.sg>
2023-12-01 13:46:20 +0800

    Added Blazor List Pages with Delete function

---
+++ CarRentalManagement/Client/Static/Endpoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly String MakesEndpoint = $"{Prefix}/makes";
+        public static readonly String ModelsEndpoint = $"{Prefix}/models";
+        public static readonly String ColoursEndpoint = $"{Prefix}/colours";
+        public static readonly String VehiclesEndpoint = $"{Prefix}/vehicles";
+        public static readonly String BookingsEndpoint = $"{Prefix}/bookings";
+        public static readonly String CustomersEndpoint = $"{Prefix}/customers";
+    }
+}
